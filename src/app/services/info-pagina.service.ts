1949c5513234a15513e0f2bf9fa532fa8347a497
Juan Jose Jaime Murga <juanjose_al_13@hotmail.com>
2024-11-05 19:58:03 -0500

    Fin sección 4

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada: boolean = false;
+
+  constructor( private http: HttpClient) { 
+    this.http.get('assets/data/data-pagina.json').subscribe( (resp: InfoPagina) => {
+      this.info = resp;
+      this.cargada = true;
+      console.log(resp);
+    });
+  }
+}
